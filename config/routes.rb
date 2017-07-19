5b255ff86a778113e40ec95a85f36069b712e5ea
Jan Siqueira <janksiqueira@gmail.com>
2017-07-19 20:32:46 -0300

    Fix routes
    Now short URIs with slashes are accepted.

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # This route sends requests to our naked url to the *cool* action in the *gif* controller.
-  root to: 'gif#cool'
+  # Main page (to be implemented)
+  # root to: ''
   
-  # I've created a gif controller so I have a page I can secure later. 
-  # This is optional (as is the root to: above).
-  resources :shortened_urls, path: 'my-urls'
+  # Signup routes
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
-  get '/cool' => 'gif#cool'
-  get '/sweet' => 'gif#sweet'
-
-  # these routes are for showing users a login form, logging them in, and logging them out.
+  # Login/logout routes
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-  # These routes will be for signup. The first renders a form in the browse, the second will 
-  # receive the form and create a user in our database using the data given to us by the user.
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
+  # Admin page for URL CRUD
+  resources :shortened_urls, path: 'my-urls'
 
-  get '/:short_uri', to: 'redirect#index'
+  # Anything else should be a reference to a shortened url
+  get '/*short_uri', to: 'redirect#index'
 end
