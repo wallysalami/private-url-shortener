aa259d6a8affee1eae2fb7ae58401efa18f7f4e3
Jan Siqueira <janksiqueira@gmail.com>
2017-07-21 18:38:43 -0300

    Add validation for shortened urls

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,3 +1,7 @@
 class ShortenedUrl < ApplicationRecord
   belongs_to :user
+  validates :short_uri, :uniqueness => true, :presence => true
+  validates :destination_url, :presence => true
+  validates_format_of :short_uri, :with => URI::DEFAULT_PARSER.regexp[:REL_URI]
+  validates_format_of :destination_url, :with => URI::DEFAULT_PARSER.regexp[:ABS_URI] #URI::regexp(%w(http https))
 end
