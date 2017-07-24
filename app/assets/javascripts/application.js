edacf26c5b3fb040dda9c4f5986bd52bb97aa887
Jan Siqueira <janksiqueira@gmail.com>
2017-07-23 21:11:18 -0300

    Upgrade to Bootstrap 4 (alpha 6)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require turbolinks
 //= require_tree .
 //= require jquery
-//= require qrcode.min
+//= require tether.min
 //= require bootstrap.min
+//= require qrcode.min
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Remove Rails field_with_errors wrapper
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
